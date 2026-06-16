62e42c9134bb228830a40a118e0622342a826e7b
Chris Collis <collisdigital@users.noreply.github.com>
2026-06-16 13:16:26 +0000

    Change app icon to Lucide clipboard-list

--- apps/frontend/src/routes/+layout.svelte
+++ apps/frontend/src/routes/+layout.svelte
@@ -46,9 +46,12 @@
 						aria-hidden="true"
 						focusable="false"
 					>
-						<path d="M4 4h11l5 5v11a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1Z" />
-						<path d="M14 4v6h6" />
-						<path d="M8 14h8M8 18h5" />
+						<rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
+						<path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
+						<path d="M12 11h4" />
+						<path d="M12 16h4" />
+						<path d="M8 11h.01" />
+						<path d="M8 16h.01" />
 					</svg>
 					<span class="sr-heading-s text-fg-inverse">Case Note Tracking</span>
 				</div>
